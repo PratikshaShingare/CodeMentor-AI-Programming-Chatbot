dfd506c982ad81b1db206f640dbbdc96584105a9
Pratiksha Mahendra Shingare <pratikshas736@gmail.com>
2026-07-12 14:00:37 +0530

    Update backend.py

--- backend.py
+++ backend.py
@@ -391,5 +391,8 @@ def get_suggestions():
     ]
     return jsonify({'suggestions': suggestions})
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000, host='0.0.0.0')
+import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
